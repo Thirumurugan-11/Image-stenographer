bd9674df597b92ec62a92f0900cfc5aed7adccfc
Thirumurugan-11 <a.thirumurugan13943@gmail.com>
2023-04-24 14:01:54 +0530

    All files

---
+++ web.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import cv2 
+import numpy as np
+import pandas as pd
+
+st.header("Image StenoGrapher")
+uploaded_file = st.file_uploader("Choose a image :)  ")
+sbop1 = st.sidebar.button("Encrypt ")
+#st.write("Enter your DATA to encrypt : ")
+
+
+if sbop1:
+    st.write("Enter your DATA to encrypt : ")
+
+
+
+
+
